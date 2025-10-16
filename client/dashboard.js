652ebe9e8a4d2b3204f261ced5d01f1b4f14c88d
Ananya <ananyabera@gmail.com>
2025-10-16 09:32:09 +0530

    Updated Dashboard

---
+++ client/dashboard.js
@@ -0,0 +1,11 @@
+function goProfile() {
+      window.location.href = "profile.html";
+    }
+
+function openPanel(){
+     document.getElementById("sidePanel").style.width = "250px";
+
+}
+function closePanel() {
+  document.getElementById("sidePanel").style.width = "0";
+}
