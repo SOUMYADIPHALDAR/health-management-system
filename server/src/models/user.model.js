364a5dc8dc12e367ca952588df6aff2d971770dc
Soumya <soumyadiphaldar3@gmail.com>
2025-10-16 21:30:02 +0530

    add refresh and access tokens

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -35,7 +35,32 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
-}
+};
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKE_EXPIRY
+        }
+    )
+};
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
