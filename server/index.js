eea79835f363ec59f181396131c626c87743126a
Soumya <soumyadiphaldar3@gmail.com>
2025-10-17 20:41:04 +0530

    add multer and cloudinary

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./src/config/db.js");
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({extendeda: true}));
+app.use(cors());
 
 connectDB()
 .then(() => {
