b939367a57715c232095211863fba7315c42af0f
Soumya <soumyadiphaldar3@gmail.com>
2025-10-26 21:47:36 +0530

    update user controller and user route

--- server/src/routers/user.route.js
+++ server/src/routers/user.route.js
@@ -1,7 +1,12 @@
-const express = require("express")
-const router = express.Router()
-const {RegisterUser, logedinUser} = require("../controllers/user.controller.js")
-const multer = require("../middlewares/multer.middleware.js")
-router.post("/register", multer.single("avatar"), RegisterUser)
-router.post("/login", logedinUser)
+const express = require("express");
+const router = express.Router();
+const {
+  RegisterUser,
+  logInUser,
+} = require("../controllers/user.controller.js");
+const multer = require("../middlewares/multer.middleware.js");
+
+router.post("/register", multer.single("avatar"), RegisterUser);
+router.post("/login", logInUser);
+
 module.exports = router;
