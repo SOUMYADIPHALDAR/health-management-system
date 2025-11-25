31c4082380d0d380bf0efd010ff2720069da630b
Ananya <ananyabera@gmail.com>
2025-11-25 23:50:42 +0530

    updated login page

--- client/script.js
+++ client/script.js
@@ -17,6 +17,15 @@ function editProfile() {
       }
     }
 
+    function togglePassword() {
+        let pwd = document.getElementById("password");
+        if (pwd.type === "password") {
+            pwd.type = "text";
+        } else {
+            pwd.type = "password";
+        }
+      }
+
 // === edit profile page ===
 function goBack() {
       window.history.back();
